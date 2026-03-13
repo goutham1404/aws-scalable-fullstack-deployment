804328b2adeda7aaa2c1b0166fc822bfb04fa959
Kamireddy Gowtham Reddy <104155883+goutham1404@users.noreply.github.com>
2026-03-13 18:51:57 +0530

    Implement Slack notification in Lambda function

---
+++ Lambda_Test_code.py
@@ -0,0 +1,31 @@
+import json
+import os
+import urllib.re quest
+#  add environment value in lambda key=SLACK_WEBHOOK_URL, value=webhook_url
+slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+
+def lambda_handler(event, context):
+    print(event)
+
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+
+    alarm_name = message['AlarmName']
+    new_state = message['NewStateValue']
+    reason = message['NewStateReason']
+
+    slack_message = {
+        'text': "%s state is now %s: %s" % (alarm_name, new_state, reason)
+    }
+
+    req = urllib.request.Request(
+        slack_webhook_url,
+        data=json.dumps(slack_message).encode('utf-8'),
+        headers={'Content-Type': 'application/json'}
+    )
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            response_body = response.read().decode()
+            print("Message posted to Slack:", response_body)
+    except Exception as e:
+        print("Failed to send message:", str(e))
